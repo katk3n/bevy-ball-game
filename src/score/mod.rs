7a5c413a2dcd86096c3f46dbd1ad1e562e23b8cd
katk3n <kentaktwo@yahoo.com>
2023-04-14 00:12:01 +0900

    EP7: refactoring

---
+++ src/score/mod.rs
@@ -0,0 +1,19 @@
+use bevy::prelude::*;
+
+use resources::*;
+use systems::*;
+
+pub mod resources;
+mod systems;
+
+pub struct ScorePlugin;
+
+impl Plugin for ScorePlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<Score>()
+            .init_resource::<HighScores>()
+            .add_system(update_score)
+            .add_system(update_high_scores)
+            .add_system(high_scores_updated);
+    }
+}
